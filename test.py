7ce12385d960d52a748b67ccb853caafa5ac31b6
ge red <ge.redmondd@gmail.com>
2025-04-24 17:41:19 -0600

    feat(codigo.py): add main

--- test.py
+++ test.py
@@ -4,8 +4,12 @@ def imprimir(mensaje):
 def imprimirLista(l):
 	print(list(l*2))
 
+def sumar(a,b):
+	c = a+b
+	return c
+
 
 if __name__ == "__main__":
 	impimir("hola mundo")
 	imprimirLista(range(5))
-
+	print(sumar(5,4))
