144a838e8b37962bffb77c6f2ce67b21103c68c6
Yadira Fleitas <yadyguitar@yahoo.es>
2025-04-24 16:15:41 -0600

    Version 3

--- Readme.txt
+++ Readme.txt
@@ -1,1 +1,3 @@
 Inicio del taller
+no hubo algo en la versión 2
+Pero sí hay en la versión 3
--- test.py
+++ test.py
@@ -1,2 +1,5 @@
+
 if __name__ == "__main__":
 	print("Hola mundo")
+	lista = range(5)
+	print(list(lista))
