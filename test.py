0b4daeeeccac8b4dbd04ca43fa4af2fd7b3a11b9
Alessandro Atilano <alessandroah77@gmail.com>
2025-04-24 17:44:58 -0600

    ale

--- test.py
+++ test.py
@@ -4,6 +4,14 @@ def imprimir(mensaje):
 def imprimirLista(l):
 	print(list(l*2))
 
+def quicksort(arr):
+	if len(arr) <= 1:
+		return arr
+	else:
+		pivot = arr[0]
+		left = [x for x in arr[1:] if x < pivot]
+		right = [x for x in arr[1:] if x >= pivot]
+		return quicksort(left) + [pivot] + quicksort(right)
 
 if __name__ == "__main__":
 	impimir("hola mundo")
